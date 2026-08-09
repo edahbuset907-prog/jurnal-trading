b31a3a3002bcf1926bef43a9692389d086ab51d8
edahbuset907-prog <edahbuset907@gmail.com>
2026-08-09 09:29:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -242,6 +242,13 @@ if not df_sesi.empty and "sesi" in df_sesi.columns:
       .reset_index()
   )
   st.dataframe(sesi_summary, use_container_width=True)
+    # --- UPDATE SQL DATABASE ---
+try:
+  cursor.execute("ALTER TABLE trades ADD COLUMN sesi TEXT")
+  conn.commit()
+except Exception as e:
+  pass
+    
     
     
     
